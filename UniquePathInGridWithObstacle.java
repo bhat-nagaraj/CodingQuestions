680a3e4144008f2b3032f4d21e42ae0a9c6a0753
bhat-nagaraj <94622372+bhat-nagaraj@users.noreply.github.com>
2023-07-22 11:34:28 +0530

    Update UniquePathInGridWithObstacle.java

--- UniquePathInGridWithObstacle.java
+++ UniquePathInGridWithObstacle.java
@@ -1,6 +1,6 @@
 //Given a grid of size m * n, let us assume you are starting at (1, 1) and your goal is to reach (m, n). At any instance, if you are on (x, y), you can either go to (x, y + 1) or (x + 1, y).
-Now consider if some obstacles are added to the grids. How many unique paths would there be?
-An obstacle and empty space are marked as 1 and 0 respectively in the grid.
+//Now consider if some obstacles are added to the grids. How many unique paths would there be?
+//An obstacle and empty space are marked as 1 and 0 respectively in the grid.
 
 
 import java.util.*;
@@ -24,28 +24,36 @@ public class Main
         int ans[][]=new int[m][n];
         ans[0][0]=0;
         for(int i=1;i<m;i++){
-            if(arr[i-1][0]!=1)
-            ans[i][0]=1;
+            if(arr[i-1][0]!=0 || arr[i][0]!=0)
+            ans[i][0]=-1;
             else
-            ans[i][0]=Integer.MAX_VALUE;
+            ans[i][0]=1;
         }
         for(int j=1;j<n;j++){
-            if(arr[0][j-1]!=1)
-            ans[0][j]=1;
+            if(arr[0][j-1]!=0 || arr[0][j]!=0)
+            ans[0][j]=-1;
             else
-            ans[0][j]=Integer.MAX_VALUE;
+            ans[0][j]=1;
         }
         
         for(int i=1;i<m;i++)
         {
             for(int j=1;j<n;j++)
             {
-                if(arr[i-1][j]!=1 && arr[i][j-1]!=1)
+                if(arr[i][j]==1)
+                ans[i][j]=-1;
+                else 
+                {
+                if(ans[i-1][j]!=-1 && ans[i][j-1]!=-1)
                 ans[i][j]=ans[i-1][j]+ans[i][j-1];
-                else if(arr[i-1][j]!=1 && arr[i][j-1]==1)
+                else if(ans[i-1][j]!=-1 && ans[i][j-1]==-1)
                 ans[i][j]=ans[i-1][j];
-                else if(arr[i-1][j]==1 && arr[i][j-1]!=1)
+                else if(ans[i-1][j]==-1 && ans[i][j-1]!=-1)
                 ans[i][j]=ans[i][j-1];
+                else
+                ans[i][j]=-1;
+                }
+                
             }
         }
         for(int i=0;i<m;i++){
